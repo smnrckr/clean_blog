290cc07b501af0d70f3e5ce723df0ab1c141078d
Semanur Çakır <semanurcakir11@gmail.com>
2023-12-10 00:10:18 +0300

    imlement routes

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app=express();
 const port=3000;
-const blog = { id: 1, title: "Blog title", description: "Blog description" }
-app.get('/',(req,res)=>{
-    res.send(blog);
+//Template Engine
+app.set("view engine", "ejs");
+//middleware
+app.use(express.static('public'));
+
+app.get("/",(req,res)=>{
+    res.render('index');
+});
+app.get("/about",(req,res)=>{
+    res.render('about');
+});
+app.get("/add_post",(req,res)=>{
+    res.render('add_post');
+});
+app.get("/post",(req,res)=>{
+    res.render('post');
 });
 
+
 app.listen(port,(req,res)=>{
-    console.log(`server ${port} portunda başlatıldı`);
+    console.log(`server ${port} portunda baslatildi`);
 
 });
